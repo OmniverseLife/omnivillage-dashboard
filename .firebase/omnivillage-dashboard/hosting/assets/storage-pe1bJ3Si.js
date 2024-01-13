3edc933ba590e767f83853c14b945b8a9b80de22
Anas Alam <anasalam027@gmail.com>
2024-01-14 01:03:18 +0530

    changes

---
+++ .firebase/omnivillage-dashboard/hosting/assets/storage-pe1bJ3Si.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-cS6WM9XH.js";import{P as o}from"./production--N6_QEAx.js";function r(){return a.jsxDEV(o,{},void 0,!1,{fileName:"/Users/aannaassalam/Documents/Github/omni-village-dashboard/src/layout/pages/production/storage.jsx",lineNumber:5,columnNumber:10},this)}export{r as default};
--- src/layout/pages/crops/poultry.jsx
+++ src/layout/pages/crops/poultry.jsx
@@ -55,7 +55,7 @@ function Poultry() {
       width: 200,
       renderCell: (params) => (
         <Typography variant="body2" sx={{ textTransform: "capitalize" }}>
-          {params.row.label?.name}
+          {params.row.label?.name?.en}
         </Typography>
       ),
     },
